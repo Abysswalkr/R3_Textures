0e8920d5589f3c398ea3c4be1e53f351ec04a4e3
José Gramajo <gra22907@uvg.edu.gt>
2024-08-07 22:39:27 -0600

    Update vertex and fragment shaders for 3D model support and texturing

--- shaders.py
+++ shaders.py
@@ -1,50 +1,31 @@
-
 def vertexShader(vertex, **kwargs):
-    # Se lleva a cabo por cada vertice
-    
-    # Recibimos las matrices
     modelMatrix = kwargs["modelMatrix"]
     viewMatrix = kwargs["viewMatrix"]
     projectionMatrix = kwargs["projectionMatrix"]
     viewportMatrix = kwargs["viewportMatrix"]
-    
-    # Agregamos un componente W al vertice
-    vt = [vertex[0],
-          vertex[1],
-          vertex[2],
-          1]
-    
-    # Transformamos el vertices por todas las matrices en el orden correcto
-    vt = viewportMatrix * projectionMatrix * viewMatrix * modelMatrix @ vt
-    
+
+    vt = [vertex[0], vertex[1], vertex[2], 1]
+
+    vt = viewportMatrix @ projectionMatrix @ viewMatrix @ modelMatrix @ vt
     vt = vt.tolist()[0]
-    
-    # Dividimos x,y,z por w para regresar el vertices a un tamaño de 3
-    vt = [vt[0] / vt[3],
-          vt[1] / vt[3],
-          vt[2] / vt[3]]
-    
+
+    vt = [vt[0] / vt[3], vt[1] / vt[3], vt[2] / vt[3]]
+
     return vt
 
 
 def fragmentShader(**kwargs):
-    # Se lleva a cabo por cada pixel individual
-    
-    # Obtenemos la informacion requerida
     A, B, C = kwargs["verts"]
     u, v, w = kwargs["bCoords"]
-    
-    
-    # Empezamos siempre con color blanco
-    r = 1
-    g = 1
-    b = 1
-    
-    # Para el proposito de mostrar las coordenadas de textura
-    # en accion, las usamos para el color
-    r *= u
-    g *= v
-    b *= w
-        
-    # Se regresa el color
-    return [r,g,b]
+    texture = kwargs["texture"]
+    texCoords = kwargs["texCoords"]
+
+    tx = texCoords[0][0] * u + texCoords[1][0] * v + texCoords[2][0] * w
+    ty = texCoords[0][1] * u + texCoords[1][1] * v + texCoords[2][1] * w
+
+    texColor = texture.getColor(tx, ty)
+
+    if texColor:
+        return texColor
+    else:
+        return [1, 1, 1]
